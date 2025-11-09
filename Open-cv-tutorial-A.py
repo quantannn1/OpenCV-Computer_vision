86dfb803aa242112deeaeef979cc0b04f449004c
quantannn1 <tanszequan@gmail.com>
2025-11-09 20:28:57 +0800

    Uploaded the code
    Uploaded newest code

---
+++ Open-cv-tutorial-A.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread(r'<Your file name/directory>')
+
+cv.imshow('<name of the img>', img)
+
+#Transformation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+# -X = left
+# -Y = up
+# x = Right
+# y = Down
+
+translated = translate(img, -60,70)
+cv.imshow('Translated', translated)
+
+#Rotation
+
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(img, -90)
+cv.imshow('Rotated Rotated' , rotated_rotated)
+
+#Resizeing
+
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_LINEAR)
+cv.imshow('Resized', resized)
+
+#Flipping
+
+flip = cv.flip(img, -1)
+cv.imshow('Flip', flip)
+
+#Cropping
+cropped = img[100:200, 30:400]
+cv.imshow('Cropped', cropped)
+
+cv.waitKey(0)
